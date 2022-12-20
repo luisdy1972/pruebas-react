7cd1a6c879395357498b5432f737aa28371fb73b
luisdy1972 <luisdy1972@hotmail.com>
2022-12-20 11:01:00 -0500

    con css

--- src/App.js
+++ src/App.js
@@ -6,18 +6,39 @@ function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<Card
-				nombre="Luis Ayala"
-				cargo="Auxiliar de sistema"
-				presentacion='"Los técnicos laborales como Auxiliares de Sistemas Informáticos operan equipos de cómputo, monitorean la operación de sistemas de computación y redes, coordinan su uso y producen reportes de operación."'
-				link="https://jw.org"
-			/>
-			<Card
-				nombre="Luis Ayala"
-				cargo="Auxiliar de sistema"
-				presentacion='"Los técnicos laborales como Auxiliares de Sistemas Informáticos operan equipos de cómputo, monitorean la operación de sistemas de computación y redes, coordinan su uso y producen reportes de operación."'
-				link="https://jw.org"
-			/>
+			<h2 className="hola">Hola Mundo</h2>
+			<div className="Feed">
+				<Card
+					nombre="Luis Ayala"
+					cargo="Auxiliar de sistema"
+					presentacion='"Los técnicos laborales como Auxiliares de Sistemas Informáticos operan equipos de cómputo, monitorean la operación de sistemas de computación y redes, coordinan su uso y producen reportes de operación."'
+					link="https://jw.org"
+				/>
+				<Card
+					nombre="Luis Ayala"
+					cargo="Auxiliar de sistema"
+					presentacion='"Los técnicos laborales como Auxiliares de Sistemas Informáticos operan equipos de cómputo, monitorean la operación de sistemas de computación y redes, coordinan su uso y producen reportes de operación."'
+					link="https://jw.org"
+				/>
+				<Card
+					nombre="Luis Ayala"
+					cargo="Auxiliar de sistema"
+					presentacion='"Los técnicos laborales como Auxiliares de Sistemas Informáticos operan equipos de cómputo, monitorean la operación de sistemas de computación y redes, coordinan su uso y producen reportes de operación."'
+					link="https://jw.org"
+				/>
+				<Card
+					nombre="Luis Ayala"
+					cargo="Auxiliar de sistema"
+					presentacion='"Los técnicos laborales como Auxiliares de Sistemas Informáticos operan equipos de cómputo, monitorean la operación de sistemas de computación y redes, coordinan su uso y producen reportes de operación."'
+					link="https://jw.org"
+				/>
+				<Card
+					nombre="Luis Ayala"
+					cargo="Auxiliar de sistema"
+					presentacion='"Los técnicos laborales como Auxiliares de Sistemas Informáticos operan equipos de cómputo, monitorean la operación de sistemas de computación y redes, coordinan su uso y producen reportes de operación."'
+					link="https://jw.org"
+				/>
+			</div>
 		</div>
 	)
 }
